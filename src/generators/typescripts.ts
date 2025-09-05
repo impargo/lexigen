ca0bbed98d1d68c08ae1ddcfe52acc377571b0cc
Julian Labeit <julianlabeit@gmail.com>
2025-09-05 11:08:30 +0200

    feat(mixpanel): allow also tracking server side events (#3)
    * feat(mixpanel): allow also tracking server side events
    
    * fix: identify users correctly

--- src/generators/typescripts.ts
+++ src/generators/typescripts.ts
@@ -20,14 +20,36 @@ const formatDescription = (event: EventSchema): string => {
   return event.schemaJson.description ? `/**\n * ${event.schemaJson.description}\n */\n` : ''
 }
 
-export const generate = async (events: EventSchema[]): Promise<string> => {
+export const generate = async (events: EventSchema[], serverSide: boolean): Promise<string> => {
   const formattedEvents = await Promise.all(events.map(async event => {
     const interfaceName = `${pascalCase(event.name)}Properties`
     const functionName = `track${pascalCase(event.name)}Event`
     const params = await generateParamsForEvent(event, interfaceName)
     const shouldParamsBeOptional = !Object.keys(event.schemaJson.properties ?? {}).length
     // eslint-disable-next-line max-len
+    if (serverSide) {
+      return `${params}\n${formatDescription(event)}export const ${functionName} = async (userId: string, properties${shouldParamsBeOptional ? '?' : ''}: ${interfaceName}) => mixpanel?.track('${event.name}', { distinct_id: await sha256(userId), ...properties })`
+    }
+    // eslint-disable-next-line max-len
     return `${params}\n${formatDescription(event)}export const ${functionName} = (properties${shouldParamsBeOptional ? '?' : ''}: ${interfaceName}) => mixpanel.track('${event.name}', properties)`
   }))
-  return `/* eslint-disable */\nimport mixpanel from 'mixpanel-browser'\n\n${formattedEvents.join('\n\n')}`
+  if (serverSide) {
+    return `/* eslint-disable */
+import Mixpanel from 'mixpanel'
+
+let mixpanel: Mixpanel.Mixpanel | undefined
+export const initMixpanel = (token: string) => {
+  mixpanel = Mixpanel.init(token)
+}
+const sha256 = async (str: string): Promise<string> => {
+  const buf = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(str))
+  return Array.prototype.map.call(new Uint8Array(buf), x => (('00' + x.toString(16)).slice(-2))).join('').slice(0, 16)
+}
+${formattedEvents.join('\n\n')}`
+  }
+  return `
+/* eslint-disable */
+import mixpanel from 'mixpanel-browser'
+
+${formattedEvents.join('\n\n')}`
 }
